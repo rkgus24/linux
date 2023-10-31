09cad55a162d17256c01c1ce98a3e32e40558504
가현 <102145605+rkgus24@users.noreply.github.com>
2023-10-31 13:16:51 +0900

    메세지 작성 -> "Listener" 프로세스에 알림 -> 출력, 공유 메모리 및 IPC 리소스를 정리

---
+++ chap11/ch1106c.c
@@ -0,0 +1,32 @@
+#include <signal.h>
+#include <sys/mman.h>
+#include <sys/ipc.h>
+#include <sys.shm.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <syring.h>
+
+int main(int argc, char **argv) {
+    key_t key;
+    int shmid;
+    void *shmaddr;
+    char buf[1024];
+
+    key = ftok("shmfile", 1);
+    shmid = shmget(key, 1024, 0);
+
+    shmaddr = shmat(shmid, NULL, 0);
+    strcpy(shmaddr, "Hello, I'm talker\n");
+
+    kill(atoi(argv[1]), SIGUSR1);
+    sleep(2);
+    strcpy(buf, shmaddr);
+
+    printf("Listener said : %s\n", buf);
+    system("ipcs -m");
+    shmat(shmaddr);
+    shmctl(shmid, IPC_RMID, NULL);
+
+    return 0;
+}
